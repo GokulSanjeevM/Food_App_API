9dcfcb99c5babb79988345ed6060895de4cdd18e
GokulSanjeevM <gokulsanjeev8426@gmail.com>
2024-04-16 23:53:23 +0530

    Changed apiurl and key values

--- api.js
+++ api.js
@@ -1,45 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
-app.get('/api/create-dd-monitor', (req, res) => {
+app.get("/api/create-dd-monitor", (req, res) => {
   const apiKey = process.env.DD_API_KEY;
   const appKey = process.env.DD_APP_KEY;
-    // Construct the monitor payload
-    const monitorPayload = {
-      name: "User session alert",
-      type: "metric alert",
-      query: "sum(last_5m):sum:datadog.estimated_usage.rum.sessions{application.name:recipe-app}.as_count() >= 1",
-      message: "Monitor user active sessions",
-      tags: ["env:prod", "team:backend"]
-    };
+  console.log("DD_API_KEY", apiKey);
+  console.log("DD_APP_KEY", appKey);
+  // Construct the monitor payload
+  const monitorPayload = {
+    name: "User session alert",
+    type: "metric alert",
+    query:
+      "sum(last_5m):sum:datadog.estimated_usage.rum.sessions{*}.as_count() >= 1",
+    message: "Monitor user active sessions",
+    tags: ["env:prod", "team:backend"],
+  };
 
-    // Define Datadog API endpoint for creating monitors
-    const apiUrl = "https://api.datadoghq.com/api/v1/monitor";
+  // Define Datadog API endpoint for creating monitors
+  const apiUrl = "https://api.us5.datadoghq.com/api/v1/monitor";
 
-    // Define headers for API request
-    const headers = {
-      "Content-Type": "application/json",
-      "DD-API-KEY": apiKey,
-      "DD-APPLICATION-KEY": appKey
-    };
+  // Define headers for API request
+  const headers = {
+    "Content-Type": "application/json",
+    "DD-API-KEY": apiKey,
+    "DD-APPLICATION-KEY": appKey,
+  };
 
-    // Make API request to create the monitor
-    axios
-      .post(apiUrl, monitorPayload, { headers })
-      .then((response) => {
-        res.send("Monitor created successfully:", response?.data);
-      })
-      .catch((error) => {
-        res.send(`Error creating monitor: ${JSON.stringify(error?.response?.data)}`);
-      });
+  // Make API request to create the monitor
+  axios
+    .post(apiUrl, monitorPayload, { headers })
+    .then((response) => {
+      res.send(
+        `Monitor created successfully: -${JSON.stringify(response?.data)}`
+      );
+    })
+    .catch((error) => {
+      res.send(
+        `Error creating monitor: ${JSON.stringify(error?.response?.data)}`
+      );
+    });
 });
 
-app.post('/api/datadog/webhook', (req, res) => {
+app.post("/api/datadog/webhook", (req, res) => {
   const monitorData = req.body;
-  console.log('Received monitor data check:', monitorData);
+  console.log("Received monitor data check:", monitorData);
   res.send(`data received - ${monitorData}`);
 });
 
